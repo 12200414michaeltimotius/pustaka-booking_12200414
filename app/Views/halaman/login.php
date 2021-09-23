a0b6513f6948df8225d79cacb2437ed345c44f9b
12200414michaeltimotius <michaelanakbsi@gmail.com>
2021-09-23 17:42:33 +0800

    Tugas Pertemuan 2

---
+++ app/Views/halaman/login.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Perpustakaan Login</title>
+    </head>
+    <body>
+        <h1>Selamat Datang di Sistem Informasi Perpustkaan</h1>
+        <p1>Ini adalah layanan perpustakaan Digital masa kini
+            <a href="<?=base_url( relativePath: '/daftar-member')?>">Klik Disini></a> untuk mendaftar menjadi member
+        </p1>
+        
+    </body>
+</html>
